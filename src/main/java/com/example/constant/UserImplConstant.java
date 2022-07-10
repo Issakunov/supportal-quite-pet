fd5efc3d96053339e0b39e826bb2345bb131b7b9
atabek <ataissakunov@gmail.com>
2022-07-10 23:40:20 +0600

    brute force attack implementation is completed

--- src/main/java/com/example/constant/UserImplConstant.java
+++ src/main/java/com/example/constant/UserImplConstant.java
@@ -6,5 +6,6 @@ public class UserImplConstant {
     public static final String USERNAME_ALREADY_EXISTS = "Username already exists!";
     public static final String EMAIL_ALREADY_EXISTS = "Email already exists!";
     public static final String DEFAULT_USER_IMAGE_PATH = "user/image/profile/temp";
+    public static final String NO_USER_FOUNT_BY_USERNAME = "No user fount by username";
 
 }
